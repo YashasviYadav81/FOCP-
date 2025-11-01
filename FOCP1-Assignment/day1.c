8d64a3b097ce93e3252a5f7427c84b867fd9dfe7
YashasviYadav81 <yashasviyadav609@gmail.com>
2025-11-01 12:13:53 +0530

    Add files via upload

---
+++ FOCP1-Assignment/day1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+char name= 'N';
+float age=25;
+float height=5.6;
+char g='A';
+printf("Name: %c \n",name);
+printf("Age: %.1f year old \n", age);
+printf("Height: %.2f feet \n", height);
+printf("Grade: %c",g);
+return 0;
+}
+
+
+
+
