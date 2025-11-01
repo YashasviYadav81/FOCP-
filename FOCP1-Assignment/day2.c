f076e00115caf68a05345f3a789e1a8d9edae97f
YashasviYadav81 <yashasviyadav609@gmail.com>
2025-11-01 12:11:46 +0530

    Add files via upload

---
+++ FOCP1-Assignment/day2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//int main(){
+   // int principalamount, rate, time;
+   // printf("The principal amount is ");
+   // scanf("%d", &principalamount);
+   // printf("The rate is ");
+   // scanf("%d", &rate);
+   // printf("The time is ");
+   // scanf("%d", &time);
+   // int product= principalamount*rate*time;
+   // printf("The simple interest is:%d", product/100);
+   // return 0;
+
+//}
+int main(){
+    
+}
