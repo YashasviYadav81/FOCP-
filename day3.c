4d900d35089282878ca4df42dc5ab6d99285a204
YashasviYadav81 <yashasviyadav609@gmail.com>
2025-11-01 11:54:36 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+// 1
+    // int a;
+    // int b;
+    // printf("Enter value of a:");
+    // scanf("%d", &a);
+    // printf("Enter value of b:");
+    // scanf("%d",&b);
+    // int sum= a+b;
+    // printf("The sum is: %d",sum);
+    // return 0;
+
+//2
+   /* int marks1;
+    int marks2;
+    int marks3;
+    printf("Enter value of marks1:");
+    scanf("%d", &marks1);
+    printf("Enter value of marks2:");
+    scanf("%d", &marks2);
+    printf("Enter value of marks3:");
+    scanf("%d", &marks3);
+    int sum = (marks1 + marks2 + marks3);
+    float result = sum / 3.0;
+    printf("Average is: %f", result);
+   return 0;*/
+
+//3
+  /* int radius;
+   printf("Enter value of radius");
+   scanf("%d", &radius);
+   float area= 3.14*(radius*radius);
+   printf("The area of circle is:%f", area);
+   return 0;*/
+
+//4
+   /*float farenheittemp;
+   printf("Enter value of farenheittemp");
+   scanf("%f",&farenheittemp);
+   float centigradetemp= 5.0/9.0*(farenheittemp-32);
+   printf("The centigrade temperature is: %f", centigradetemp);
+   return 0;*/
+
+//5
+   /*int height;
+   int base;
+   printf("Enter height");
+   scanf("%d", &height);
+   printf("Enter base");
+   scanf("%d", &base);
+   float area= 0.5* base* height;
+   printf("The area is: %f", area);
+   return 0;*/
+
+//6
+   /* int principalamount, rate, time;
+    printf("The principal amount is ");
+   scanf("%d", &principalamount);
+    printf("The rate is ");
+   scanf("%d", &rate);
+    printf("The time is ");
+    scanf("%d", &time);
+    int product= principalamount*rate*time;
+    printf("The simple interest is:%d", product/100);
+    return 0;*/
+
+//7
+  /* int base;
+   int perpendicular;
+   printf("Enter vale of base");
+   scanf("%d", &base);
+   printf("Enter value of perpendicular");
+   scanf("%d",&perpendicular);
+   float valuehypotenuse= sqrt(perpendicular*perpendicular+base*base);
+   printf("The value of longest side is;%f", valuehypotenuse);
+   return 0;*/
+
+//8
+   /*int i,q,u,a;
+   printf("Enter item number:");
+   scanf("%d",&i);
+   printf("Enter quantity purchased:");
+   scanf("%d",&q);
+   printf("Enter unit price:");
+   scanf("%d",&u);
+   a=q*u;
+   printf("Total Amount:%d\n",a);
+   printf("After applying discount amount will be:%f",0.8*a);
+   return 0;*/
+
+//9
+  /* int a;
+   int b;
+   int c;
+   printf("Enter a:");
+   scanf("%d",&a);
+   printf("Enter b:");
+   scanf("%d",&b);
+   c=a;
+   a=b;b=c;
+   printf("After swapping a:%d and b:%d", a,b)  ;
+   return 0; */
+
+//10 ***
+/*int a, b;
+printf("Enter a: ");
+scanf("%d", &a);
+printf("Enter b: ");
+scanf("%d", &b);
+a = a + b;
+b = a - b;
+a = a - b;
+
+printf("After swapping a: %d and b: %d\n", a, b);
+return 0;*/
+   
+//11
+/*int employees;
+int salary;
+printf("Enter no of employees:");
+scanf("%d",&employees);
+printf("Enter salary:");
+scanf("%d",&salary);
+int totalamount= employees*salary;
+printf("The total amount is:%d",totalamount);
+float bonus=(totalamount*0.12);
+printf("The bonus is:%f",bonus);
+float finalsalary=totalamount+bonus;
+printf("The new salary is:%f",finalsalary);
+return 0;*/
+
+//13***
+/*double x, y, result;
+    printf("Enter base (x): ");
+    scanf("%lf", &x);
+    printf("Enter exponent (y): ");
+    scanf("%lf", &y);
+
+    result = pow(x, y);
+
+    printf("%.2lf raised to the power %.2lf is %.2lf\n", x, y, result);
+    return 0;*/
+
+//14
+   /*int num1;
+   int num2;
+   printf("Enter value of num1:");
+   scanf("%d",&num1);
+   printf("Enter value of num2:");
+   scanf("%d",&num2);
+   if (num1>num2){
+    printf("Num1 is largest");}
+    else{
+        printf("Num2 is largest");}
+   return 0;*/
+
+//15
+ /*int num1;
+
+ printf("Enter num1:");
+   scanf("%d",&num1);
+  
+   if(num1%2==0) {
+    printf("The number is even");
+   }
+   else{
+    printf("The number is odd");
+   }
+   return 0;*/
+  
+
+}
+
